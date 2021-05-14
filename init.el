3556a1fc30868ed7ee1f67b3e55f4097b56d697e
simenojensen <simen@omholt-jensen.com>
2021-05-14 15:05:00 +0200

    Remove company box

--- init.el
+++ init.el
@@ -313,8 +313,9 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
       (apply orig-fun args)))
 
   (advice-add 'counsel-yank-pop-action :around #'vterm-counsel-yank-pop-action)
-  (setq vterm-max-scrollback 10000)
-  )
+  (setq vterm-max-scrollback 10000))
+
+(use-package multi-vterm)
 
 (use-package magit
   :bind
@@ -406,78 +407,18 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
          ("C-n" . company-select-next)
          ("C-p" . company-select-previous)))
   :config
-  (push 'company-lsp company-backends)
+  ;; (push 'company-lsp company-backends)
   (setq company-minimum-prefix-length 1)
   (setq company-idle-delay 0.0)
   (setq company-echo-delay 0.0)
-  (setq company-tooltip-idle-delay 0.0)
-  (setq company-tooltip-align-annotations t)
+  ;; (setq company-tooltip-idle-delay 0.0)
+  ;; (setq company-tooltip-align-annotations t)
   (setq company-require-match nil)
   (setq company-show-numbers t)
   (setq company-dabbrev-downcase nil) ;; case insensitive for dabbrev backend
   (global-company-mode 1)
   ;; Don't use company in debugger mode
   (setq company-global-modes '(not gud-mode)))
-
-(use-package company-box
-  :diminish
-  :hook
-  (company-mode . company-box-mode)
-  :defines company-box-icons-all-the-icons
-  :init (setq company-box-backends-colors nil
-              company-box-highlight-prefix t)
-  :config
-  (setq company-box-doc-delay 0)
-  (setq company-box-enable-icon t)
-  (setq company-box-color-icons nil)
-  (setq company-box-max-candidates 10)
-  (setq company-box-show-single-candidate t)
-  ;; all-the-icons-integration
-  (with-no-warnings
-    ;; Prettify icons
-    (defun my-company-box-icons--elisp (candidate)
-      (when (derived-mode-p 'emacs-lisp-mode)
-        (let ((sym (intern candidate)))
-          (cond ((fboundp sym) 'Function)
-                ((featurep sym) 'Module)
-                ((facep sym) 'Color)
-                ((boundp sym) 'Variable)
-                ((symbolp sym) 'Text)
-                (t . nil)))))
-    (advice-add #'company-box-icons--elisp :override #'my-company-box-icons--elisp))
-
-  (declare-function all-the-icons-faicon 'all-the-icons)
-  (declare-function all-the-icons-material 'all-the-icons)
-  (declare-function all-the-icons-octicon 'all-the-icons)
-  (setq company-box-icons-all-the-icons
-        `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.8 :v-adjust -0.15))
-          (Text . ,(all-the-icons-faicon "text-width" :height 0.8 :v-adjust -0.02))
-          (Method . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-          (Function . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-          (Constructor . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-          (Field . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-          (Variable . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-          (Class . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-          (Interface . ,(all-the-icons-material "share" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-          (Module . ,(all-the-icons-material "view_module" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-          (Property . ,(all-the-icons-faicon "wrench" :height 0.8 :v-adjust -0.02))
-          (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.8 :v-adjust -0.15))
-          (Value . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-          (Enum . ,(all-the-icons-material "storage" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-          (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.8 :v-adjust -0.15))
-          (Snippet . ,(all-the-icons-material "format_align_center" :height 0.8 :v-adjust -0.15))
-          (Color . ,(all-the-icons-material "palette" :height 0.8 :v-adjust -0.15))
-          (File . ,(all-the-icons-faicon "file-o" :height 0.8 :v-adjust -0.02))
-          (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.8 :v-adjust -0.15))
-          (Folder . ,(all-the-icons-faicon "folder-open" :height 0.8 :v-adjust -0.02))
-          (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15))
-          (Constant . ,(all-the-icons-faicon "square-o" :height 0.8 :v-adjust -0.1))
-          (Struct . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-          (Event . ,(all-the-icons-octicon "zap" :height 0.8 :v-adjust 0 :face 'all-the-icons-orange))
-          (Operator . ,(all-the-icons-material "control_point" :height 0.8 :v-adjust -0.15))
-          (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.8 :v-adjust -0.02))
-          (Template . ,(all-the-icons-material "format_align_left" :height 0.8 :v-adjust -0.15)))
-        company-box-icons-alist 'company-box-icons-all-the-icons))
 
 (defun my/jupyter-load-file ()
   "Send current buffer to jupyter kernel by default"
@@ -494,6 +435,13 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
   ;; silence indentation guesses
   (setq python-indent-guess-indent-offset-verbose nil))
 
+(use-package jupyter
+  :bind
+  (:map python-mode-map
+        ("C-c C-p" . jupyter-run-repl))
+  :init
+  (setq jupyter-repl-echo-eval-p t))
+
 (use-package lsp-python-ms
   :diminish
   :init
@@ -501,13 +449,6 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
   :config
   (setq lsp-python-ms-executable
         "~/.emacs.d/site-elisp/python-language-server/output/bin/Release/osx-x64/publish/Microsoft.Python.LanguageServer"))
-
-(use-package jupyter
-  :bind
-  (:map python-mode-map
-        ("C-c C-p" . jupyter-run-repl))
-  :init
-  (setq jupyter-repl-echo-eval-p t))
 
 (use-package conda
   :hook
@@ -772,7 +713,6 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
   :hook
   (after-save . my/tangle-emacs-config)
   (org-mode . (lambda ()
-                (setq-local company-idle-delay 0.2) ;; company idle delay
                 ;; (visual-line-mode) ;; visual line
                 (flyspell-mode) ;; turn on flyspell
                 (turn-on-org-cdlatex) ;; turn on cdlatex
@@ -891,9 +831,11 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
         '(("article"
            " \\RequirePackage{fix-cm}
   \\PassOptionsToPackage{svgnames}{xcolor}
-  \\documentclass[11pt]{article}
+  \\documentclass[10pt]{article}
   \\usepackage{fontspec}
   \\usepackage{booktabs}
+  \\usepackage{ragged2e}
+  \\usepackage[skip=2pt, justification=centering]{caption}
   \\usepackage{enumitem}
   \\usepackage[nottoc]{tocbibind}
   \\setlist{nosep,after=\\vspace{4pt}}
@@ -944,7 +886,9 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
               showstringspaces=false,
               columns=fullflexible,
               keepspaces=true}
-  \\usepackage[a4paper,top=2.5cm, bottom=2.5cm, left=2.5cm, right=2.5cm]{geometry}
+  \\usepackage[a4paper,top=1.9cm, bottom=1.9cm, left=1.32cm, right=1.32cm]{geometry}
+  %\\usepackage[none]{hyphenat}
+  \\usepackage{sectsty} % use to set section color
   \\usepackage{parskip}
   \\setlength\\parindent{0pt}
   \\setlength\\parskip{1em}
@@ -1135,6 +1079,14 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
                                    (no . ("nb_NO" "Norwegian"))))
   (setq guess-language-languages '(en no))
   (setq guess-language-min-paragraph-length 45))
+
+(use-package captain
+  :hook
+  ((text-mode . (lambda ()
+                  (setq captain-predicate (lambda () t))))
+  (org-mode . (lambda ()
+                (setq captain-predicate
+                      (lambda () (not (org-in-src-block-p))))))))
 
 (defun my/get-file-content-as-string (filePath)
   "Return filePath's content as string."
